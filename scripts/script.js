f53fc7ce998bd822dfe5a5d4e668b7357266219d
huntermahmud <mahmudthedatahunter@gmail.com>
2024-02-18 22:04:13 +0600

    total calculation done

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,37 @@
-const tickets = document.getElementsByClassName('ticket');
 let ticketCount = 0;
 let totalPrice = 0;
 const ticketPrice = 550;
 let discountPrice = 0;
 let grandTotalPrice = 0;
 
-function color(event){
-    let ticketclassList = event.target.classList;
-    
+
+const tickets = document.getElementsByClassName('ticket');
+
+
+for(let ticket of tickets){
+    ticket.addEventListener('click', action);
+}
+
+function action(event){
     if(ticketCount < 4){
         ticketCount++;
         SetInnerText("seat-cnt", ticketCount);
+        addSeatInfo(event);
+        totalPrice += ticketPrice;
+        grandTotalPrice = totalPrice;
+        SetInnerText("total-price",totalPrice);
+        SetInnerText("grand-total",grandTotalPrice);
 
-        ticketclassList.remove('bg-[#F7F8F8]');
-        ticketclassList.add('bg-[#1DD100]', 'text-white');
-        event.target.setAttribute("disabled","");
 
+        
+        if(ticketCount==4){
+            applyEnable(); // apply button enabling
+            document.getElementById("apply").addEventListener('click',checkValidation);
+        }
     }
     else{
         alert("You can't select more than 4 tickets.")
     }
     
 }
-
-for(let ticket of tickets){
-    ticket.addEventListener('click', color);
-}
-
-
 
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,16 +1,39 @@
-function SetInnerText(id,value){
-    document.getElementById(id).innerText = value;
+function SetInnerText(id, value) {
+  document.getElementById(id).innerText = value;
 }
 
-let seatInfo = document.getElementById("seat-info");
-let div = document.createElement('div');
-div.classList.add('flex','justify-between');
-let p1 = document.createElement('p');
-p1.innerText = "A1";
-let p2 = document.createElement('p');
-p2.innerText = "Economy";
-let p3 = document.createElement('p');
-p3.innerText = '550';
-div.append(p1,p2,p3);
-//seatInfo.appendChild(div);
-console.log(div);
+// apply button enable when 4 ticket selected
+function applyEnable() {
+  document.getElementById("apply").removeAttribute("disabled");
+}
+
+function checkValidation() {
+  const coupon = document.getElementById("coupon-input");
+  if (coupon.value === "NEW15") {
+    return;
+  } else if (coupon.value === "Couple 20") {
+    return;
+  } else {
+    alert("Wrong coupon code please check.");
+  }
+}
+
+function addSeatInfo(event) {
+  const id = event.target.id;
+  let ticketclassList = event.target.classList;
+  ticketclassList.remove("bg-[#F7F8F8]");
+  ticketclassList.add("bg-[#1DD100]", "text-white");
+  event.target.setAttribute("disabled", "");
+  //adding seat info to the select area
+  let seatInfo = document.getElementById("seat-info");
+  let div = document.createElement("div");
+  div.classList.add("flex", "justify-between");
+  let p1 = document.createElement("p");
+  p1.innerText = id;
+  let p2 = document.createElement("p");
+  p2.innerText = "Economy";
+  let p3 = document.createElement("p");
+  p3.innerText = "550";
+  div.append(p1, p2, p3);
+  seatInfo.appendChild(div);
+}
