bb1fbe601df2ed3a03de9b2316dc832d0158e993
huntermahmud <mahmudthedatahunter@gmail.com>
2024-02-18 17:54:06 +0600

    some javascript code added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-console.log('connected')
+document.getElementById('A1').addEventListener('click',(e)=> {
+    console.log(e.target.innerText);
+    let classList = document.getElementById(e.target.innerText).classList;
+    console.log(classList);
+    classList.remove('bg-[#F7F8F8]');
+    classList.add('bg-[#1DD100]', 'text-white')
+    console.log(classList)
+})
